30e895fb026f4190603dde509ecf43aef7b17e50
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-04-20 14:38:53 -0600

    Attempt at iteration 2

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -13,13 +13,22 @@ class FoodTruck
       @inventory[item]
     end
   end
-  # require 'pry'; binding.pry
 
   def stock(item, quantity)
     if @inventory.include?(item) == false
       @inventory[item] = quantity
     else
       @inventory[item] += quantity
+    end
+  end
+
+  #can't get last test to pass. want to try Iteration 3
+  # For method below, I need to remove the string converter into helper & call it within
+  def potential_revenue
+    @inventory.sum do |key, value|
+      key.price.slice!(0)
+      revenue = key.price.to_f * value
+      # require 'pry'; binding.pry
     end
   end
 end
